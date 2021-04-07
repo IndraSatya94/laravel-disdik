f2f1e2b59422c367a5be3c3c19c34b54775eb991
IndraSatya94 <minsatya94@gmail.com>
2021-04-07 22:00:28 +0700

    belajar laravel

---
+++ resources/views/about.blade.php
@@ -0,0 +1,13 @@
+@extends('layout/main')
+
+@section('title','about')
+
+@section('container')
+<div class="container">
+    <div class="row">
+            <div class="col 10 mt-3">
+            <h1>Hello, Indra!</h1>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layout/main')
+
+@section('title','belajar laravel')
+
+@section('container')
+<div class="container">
+    <div class="row">
+            <div class="col 10 mt-3">
+            <h1>Hello, world!</h1>
+        </div>
+    </div>
+</div>
+@endsection
